2ad0d785d27204bb835504d09f9a0e43384772d7
ameer <ameerabo11a@gmail.com>
2025-12-23 20:43:46 +0200

    add a alembic

--- project3/TodoApp/routers/users.py
+++ project3/TodoApp/routers/users.py
@@ -52,3 +52,15 @@ async def change_password(user: user_dependency, db: db_dependency,
 
     db.add(user_model)
     db.commit()
+
+
+
+@router.put("/phonenumber/{phonenumber}", status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(user: user_dependency, db: db_dependency, phone_number: str):
+
+    if user is None:
+        raise  HTTPException(status_code=401, detail='Authentication Failed')
+    user_model = db.query(Users).filter(Users.id == user.get('user_id')).first()
+    user_model.phone_number = phone_number
+    db.add(user_model)
+    db.commit()
